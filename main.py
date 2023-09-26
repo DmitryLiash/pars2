8da24ccd55975cbb7f953576f5d8400cfa1b34a4
DmitryL <dimak.pax@gmail.com>
2023-09-26 17:45:25 +0600

    ver 0002

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
 
+headers = {
+    "Accept": "*/*",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+                  "Chrome/116.0.0.0 Safari/537.36"
+}
+req = requests.get(url, headers=headers)
+src = req.text
+# print(src)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with open("index.html", "w", encoding="utf-8") as file:
+    file.write(src)
